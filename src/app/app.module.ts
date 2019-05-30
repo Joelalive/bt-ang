f06e01f42b648ba9b26672384dc05a1436da9b67
Joel <joelimalive1994@gmail.com>
2019-05-30 09:22:53 +0530

    nav fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule,
     AppRoutingModule,
     PartialsModule
   ],
--- src/app/partials/partials.module.ts
+++ src/app/partials/partials.module.ts
@@ -1,3 +1,5 @@
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './../app-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
@@ -6,11 +8,12 @@ import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     HeaderComponent,
-    FooterComponent
+    FooterComponent    
   ]
 })
 export class PartialsModule { }
